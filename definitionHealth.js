484c746f31a55fdf396b7717a9c4e80cc8bf1731
Maksim Lazarev <felshines@gmail.com>
2023-03-19 19:22:05 +0300

    .gitignore is now working

--- definitionHealth.js
+++
@@ -1,9 +0,0 @@
-export default function definitionHealth(health) {
-  if (health > 50) {
-    return 'healthy';
-  }
-  if (health <= 50 && health >= 15) {
-    return 'wounded';
-  }
-  return 'critical';
-}
